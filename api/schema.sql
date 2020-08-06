35c4290bad8b5f5f20f5b1e50bb52077285471c2
Muhammad Juan Akbar <mail@mjuan.info>
2020-08-07 00:24:22 +0200

    add db schema

--- api/schema.sql
+++ api/schema.sql
@@ -1,21 +1,25 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS organisations;
-DROP TABLE IF EXISTS events;
+
 DROP TABLE IF EXISTS users_events;
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS organisations;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id VARCHAR(30) NOT NULL,
+    id VARCHAR(50) NOT NULL,
     full_name VARCHAR(150) NOT NULL,
     email VARCHAR(150) NOT NULL,
     phone_number VARCHAR(150) NOT NULL,
     city VARCHAR(150) NOT NULL,
     country VARCHAR(150) NOT NULL,
+    remove_after_14days INTEGER NOT NULL DEFAULT 0,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-     PRIMARY KEY (id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE organisations (
-    id VARCHAR(30) NOT NULL,
+    id VARCHAR(50) NOT NULL,
     name VARCHAR(150) NOT NULL,
     description TEXT,
     address VARCHAR(150) NOT NULL,
@@ -26,8 +30,8 @@ CREATE TABLE organisations (
 );
 
 CREATE TABLE events (
-    id VARCHAR(30) NOT NULL,
-    organisation_id VARCHAR(30) NOT NULL,
+    id VARCHAR(50) NOT NULL,
+    organisation_id VARCHAR(50) NOT NULL,
     title TEXT NOT NULL,
     max_guest INTEGER NOT NULL,
     start_date TEXT NOT NULL,
@@ -43,8 +47,8 @@ CREATE TABLE events (
 
 CREATE TABLE users_events (
     id INTEGER NOT NULL AUTO_INCREMENT,
-    event_id VARCHAR(30) NOT NULL,
-    user_id VARCHAR(30) NOT NULL,
+    event_id VARCHAR(50) NOT NULL,
+    user_id VARCHAR(50) NOT NULL,
     is_attend INTEGER NOT NULL DEFAULT 0,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
